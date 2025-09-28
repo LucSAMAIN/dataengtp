90e10ce79a8dd2b3ac764a6ae61275feff51d439
riccardotommasini <riccardo.tommasini@polimi.it>
2025-09-28 23:46:32 +0200

    updated airflow for practice

--- airflow/dags/first_dag_stub.py
+++ airflow/dags/first_dag_stub.py
@@ -1,60 +1,28 @@
-import airflow
-import datetime
+import pendulum
+from datetime import timedelta
+
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.empty import EmptyOperator
 
-default_args_dict = {
-    'start_date': airflow.utils.dates.days_ago(0),
-    'concurrency': 1,
-    'schedule_interval': None,
-    'retries': 1,
-    'retry_delay': datetime.timedelta(minutes=5),
-}
+START_DATE = pendulum.datetime(2024, 1, 1, tz="UTC")
 
-first_dag = DAG(
-    dag_id='first_dag_stub',
-    default_args=default_args_dict,
+with DAG(
+    dag_id="first_dag_stub",
+    start_date=START_DATE,
+    schedule=None,          # no schedule
     catchup=False,
-)
+    max_active_tasks=1,     # old 'concurrency'
+    default_args={
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+    },
+    tags=["stub"],
+) as dag:
 
-task_one = BashOperator(
-    task_id='get_spreadsheet',
-    dag=first_dag,
-    bash_command="curl https://www.lutemusic.org/spreadsheet.xlsx --output /opt/airflow/dags/{{ds_nodash}}.xlsx",
-)
+    get_spreadsheet = EmptyOperator(task_id="get_spreadsheet")
+    transmute_to_csv = EmptyOperator(task_id="transmute_to_csv")
+    time_filter = EmptyOperator(task_id="time_filter")
+    load = EmptyOperator(task_id="load")
+    cleanup = EmptyOperator(task_id="cleanup")
 
-task_two = BashOperator(
-    task_id='transmute_to_csv',
-    dag=first_dag,
-    trigger_rule='all_success',
-    bash_command="xlsx2csv /opt/airflow/dags/{{ds_nodash}}.xlsx > /opt/airflow/dags/{{ds_nodash}}_correct.csv"
-)
-
-task_three = BashOperator(
-    task_id='time_filter',
-    dag=first_dag,
-    trigger_rule='all_success',
-    bash_command="awk -F, 'int($31) > 1588612377' /opt/airflow/dags/{{ds_nodash}}_correct.csv > /opt/airflow/dags/{{ds_nodash}}_correct_filtered.csv",
-
-)
-
-task_four = BashOperator(
-    task_id='load',
-    dag=first_dag,
-    trigger_rule='all_success',
-    bash_command="echo \"loaded\""
-)
-
-
-task_five = BashOperator(
-    task_id='cleanup',
-    dag=first_dag,
-    bash_command="rm /opt/airflow/dags/{{ds_nodash}}_correct.csv /opt/airflow/dags/{{ds_nodash}}_correct_filtered.csv /opt/airflow/dags/{{ds_nodash}}.xlsx",
-    )
-
-
-task_one >> task_two >> task_three >> task_four >> task_five
-
-
-
+    get_spreadsheet >> transmute_to_csv >> time_filter >> load >> cleanup
--- airflow/dags/second_dag_stub.py
+++ airflow/dags/second_dag_stub.py
@@ -1,100 +1,45 @@
-import airflow
-import datetime
-import urllib.request as request
-import pandas as pd
+import pendulum
+from datetime import timedelta
+
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.empty import EmptyOperator
 
-default_args_dict = {
-    'start_date': datetime.datetime(2020, 6, 25, 0, 0, 0),
-    'concurrency': 1,
-    'schedule_interval': "0 0 * * *",
-    'retries': 1,
-    'retry_delay': datetime.timedelta(minutes=5),
-}
+START_DATE = pendulum.datetime(2024, 1, 1, tz="UTC")
 
-second_dag = DAG(
-    dag_id='second_dag_stub',
-    default_args=default_args_dict,
-    catchup=False
-)
+with DAG(
+    dag_id="second_dag_stub",
+    start_date=START_DATE,
+    schedule="0 0 * * *",   # daily at 00:00 UTC
+    catchup=False,
+    max_active_tasks=1,     # replaces old DAG-level 'concurrency'
+    default_args={
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+    },
+    template_searchpath=["/opt/airflow/dags/"],
+    tags=["stub"],
+) as dag:
 
-task_one = DummyOperator(
-    task_id='get_spreadsheet',
-    dag=second_dag,
-    trigger_rule='all_success',
-    depends_on_past=False,
-)
+    get_spreadsheet = EmptyOperator(task_id="get_spreadsheet")
+    transmute_to_csv = EmptyOperator(task_id="transmute_to_csv")
+    time_filter = EmptyOperator(task_id="time_filter")
 
-task_two = DummyOperator(
-    task_id='transmute_to_csv',
-    dag=second_dag,
-    trigger_rule='all_success',
-    depends_on_past=False,
-)
+    emptiness_check = EmptyOperator(task_id="emptiness_check")
+    split = EmptyOperator(task_id="split")
 
-task_three = DummyOperator(
-    task_id='time_filter',
-    dag=second_dag,
-    trigger_rule='all_success',
-    depends_on_past=False,
-)
+    create_intavolature_query = EmptyOperator(task_id="create_intavolature_query")
+    create_composer_query = EmptyOperator(task_id="create_composer_query")
 
-task_four = DummyOperator(
-    task_id='emptiness_check',
-    dag=second_dag,
-    trigger_rule='all_success',
-    depends_on_past=False,
-)
+    insert_intavolature_query = EmptyOperator(task_id="insert_intavolature_query")
+    insert_composer_query = EmptyOperator(task_id="insert_composer_query")
 
-task_five = DummyOperator(
-    task_id='split',
-    dag=second_dag,
-    trigger_rule='all_success',
-    depends_on_past=False
-)
+    join_tasks = EmptyOperator(task_id="coalesce_transformations")
+    end = EmptyOperator(task_id="end")
 
-task_six_a = DummyOperator(
-    task_id='create_intavolature_query',
-    dag=second_dag,
-    trigger_rule='all_success',
-    depends_on_past=False
-)
-
-task_six_b = DummyOperator(
-    task_id='create_composer_query',
-    dag=second_dag,
-    trigger_rule='all_success',
-    depends_on_past=False,
-)
-
-task_seven_a = DummyOperator(
-    task_id='insert_intavolature_query',
-    dag=second_dag,
-    trigger_rule='all_success',
-    depends_on_past=False
-)
-
-task_seven_b = DummyOperator(
-    task_id='insert_composer_query',
-    dag=second_dag,
-    trigger_rule='all_success',
-    depends_on_past=False
-)
-
-join_tasks = DummyOperator(
-    task_id='coalesce_transformations',
-    dag=second_dag,
-    trigger_rule='none_failed',
-    depends_on_past=False
-)
-
-end = DummyOperator(
-    task_id='end',
-    dag=second_dag,
-    trigger_rule='none_failed',
-    depends_on_past=False
-)
+    # Graph (structure preserved)
+    get_spreadsheet >> transmute_to_csv >> time_filter >> emptiness_check
+    emptiness_check >> [split, end]
+    split >> [create_intavolature_query, create_composer_query]
+    create_intavolature_query >> insert_intavolature_query
+    create_composer_query >> insert_composer_query
+    [insert_intavolature_query, insert_composer_query] >> join_tasks >> end
